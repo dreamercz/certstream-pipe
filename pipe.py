d26451bf6683363aa2b4cead9e7c690557cfab6b
dusan.haustein@intraworlds.com <dusan.haustein@intraworlds.com>
2018-04-01 12:18:50 +0200

    Add outline of functions

---
+++ pipe.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script takes piped input, filters and processes it, and displays statistics about the piped data.
+Based on the workshop assigment from: https://github.com/intraworlds/workshop-php
+"""
+
+
+def take_piped_input(parameter_list):
+    pass
+
+
+def retrieve_line(parameter_list):
+    pass
+
+
+def filter_domain_name(parameter_list):
+    pass
+
+
+def create_statistics(parameter_list):
+    pass
+
+
+def display_statistics(parameter_list):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
